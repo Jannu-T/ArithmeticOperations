8e193667f27bf985ffab47679ac3ff69efcf7887
sangeetha <sangeethamuthupandiyan8545@gmail.com>
2023-07-13 09:57:48 +0530

    edited.. by Sangee-M

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 import java.lang.*;
 
 public class Main {
-    public int add(int a, int b)
+    public BigDecimal add(BigDecimal b1,BigDecimal b2)
     {
-        return (a+b);
+        return b1.add(b2);
     }
-    public int sub(int a, int b)
+    public BigDecimal sub(BigDecimal b1,BigDecimal b2)
     {
-        return (a-b);
+        return b1.subtract(b2);
     }
     public static void main(String[] args)
     {
         Main obj = new Main();
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter value for a : ");
-        int a= Integer.parseInt(scanner.next());
+        String a= scanner.next();
         System.out.print("Enter value for b : ");
-        int b= Integer.parseInt(scanner.next());
-        System.out.println("Addition of a and b = "+obj.add(a,b));
-        System.out.println("Subtraction of b from a = "+obj.sub(a,b));
+        String b= scanner.next();
+        BigDecimal big1=new BigDecimal(a);
+        BigDecimal big2=new BigDecimal(b);
+        System.out.println("Addition of a and b = "+obj.add(big1,big2));
+        System.out.println("Subtraction of b from a = "+obj.sub(big1,big2));
     }
 }
