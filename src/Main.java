287b42962062b22d01f6ebf07e1eb0b2788be9d2
Jannu-T <jannuthiyagu@gmail.com>
2023-07-13 09:55:39 +0530

    updated some methods

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
-import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
-import java.lang.*;
-
+import java.math.BigDecimal;
 public class Main {
-    public int add(int a, int b)
-    {
-        return (a+b);
+    //Multiplication
+    public BigDecimal mul(BigDecimal a,BigDecimal b){
+        BigDecimal product=a.multiply(b);
+        return product;
     }
-    public int sub(int a, int b)
-    {
-        return (a-b);
+    //Division
+    public BigDecimal div(BigDecimal a,BigDecimal b){
+        BigDecimal division=a.divide(b,2,RoundingMode.HALF_UP);
+        return division;
     }
-    public static void main(String[] args)
-    {
-        Main obj = new Main();
+    public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+
         System.out.print("Enter value for a : ");
-        int a= Integer.parseInt(scanner.next());
+        BigDecimal a = new BigDecimal(scanner.nextLine());
+
         System.out.print("Enter value for b : ");
-        int b= Integer.parseInt(scanner.next());
-        System.out.println("Addition of a and b = "+obj.add(a,b));
-        System.out.println("Subtraction of b from a = "+obj.sub(a,b));
+        BigDecimal b = new BigDecimal(scanner.nextLine());
+
+        Main obj = new Main();
+        System.out.println("Multiplication of a and b = "+obj.mul(a,b));
+        System.out.println("Division of b from a = "+obj.div(a,b));
     }
 }
